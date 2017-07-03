34e6e194fb49c1179db074e55097e5de6463b6de
李磊 <1369183590@qq.com>
2017-07-03 19:29:12 +0800

    add:添加视图动画

---
+++ EasyARObject/Assets/DelayPlay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class DelayPlay : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("PlaySound", 2f);
+    }
+
+    void PlaySound()
+    {
+        this.GetComponent<AudioSource>().Play();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
